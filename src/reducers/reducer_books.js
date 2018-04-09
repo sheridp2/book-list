b2aa46c649af8a1f770b231f6e079acc06d28531
Patrick Sheriddan <sheridp2@gmail.com>
2018-04-08 19:34:21 -0700

    redux starter

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default funtion(){
+  return[
+    {title: 'JS is cool'},
+    {title: 'Harry Potter'},
+    {title: 'Doom'},
+    {title: 'Halo'},
+  ]
+}
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+'use strict';
+require('dotenv').config();
+const {DefinePlugin} = require('webpack')
+
+let plugins = [
+  new DefinePlugin({
+    __API_URL__: JSON.stringify(process.env.API_URL),
+  }),
+]
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
+};
